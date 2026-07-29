96008e44c78b7d2c8a5185d711309e875475c34d
abdulkadir <toedue17@gmail.com>
2026-07-29 17:42:51 +0300

    july 2026

--- react/Create_list/src/Iphone.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function Iphone() {
-  return (
-    <div>Iphone is awsome</div>
-  )
-}
---
+++ react/practice/src/components/Welcome.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Welcome() {
+  return (
+   <h2>Welcome to my blog</h2>
+  )
+}
+
+export default Welcome
